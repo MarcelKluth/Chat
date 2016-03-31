665d6531360bc382a7651d65563d8c385559dabc
JoPa <JoPa@DESKTOP-SCAVOJI.fritz.box>
2016-03-31 17:16:32 +0200

    hallo

---
+++ Test/src/ff/Peter.java
@@ -0,0 +1,11 @@
+package ff;
+
+public class Peter {
+    public static void main(String args[]) {
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println(i);
+        }
+
+    }
+}

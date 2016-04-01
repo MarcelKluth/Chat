12796a9f38fc112371c6ff3f47f152e9fee5b5e5
Jonas <luise@10.24.215.18>
2016-04-01 10:53:12 +0200

    TESTNR3

--- Test/src/ff/Peter.java
+++ Test/src/ff/Peter.java
@@ -3,7 +3,7 @@ package ff;
 public class Peter {
     public static void main(String args[]) {
 
-        for (int i = xyhjuihjd0; i < 6; i++) {
+        for (int i = xyhjuihjd0; i < 100; i++) {
             System.out.println(i);
             
             Test
